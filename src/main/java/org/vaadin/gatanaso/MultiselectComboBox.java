fc8335b22e4397e0b5736bb70a5abde462ada87c
Goran <gatanaso@users.noreply.github.com>
2019-10-03 18:53:24 +0300

    increased multiselect-combo-box dependency version to fix `focusedIndex`
    Storing the focusedIndex on value changes had a side effect that after
    clearing the items the focusedIndex is unchanged and the last
    selected value becomes re-selected. To remedy this issue, the
    multiselect-combo-box now resets the focusedIndex to -1 whenever the
    value is cleared.

--- src/main/java/org/vaadin/gatanaso/MultiselectComboBox.java
+++ src/main/java/org/vaadin/gatanaso/MultiselectComboBox.java
@@ -81,7 +81,7 @@ import elemental.json.JsonValue;
  * @author gatanaso
  */
 @Tag("multiselect-combo-box")
-@NpmPackage(value = "multiselect-combo-box", version = "2.0.3-alpha.2")
+@NpmPackage(value = "multiselect-combo-box", version = "2.0.3-alpha.3")
 @JsModule("multiselect-combo-box/src/multiselect-combo-box.js")
 @JavaScript("frontend://multiselectComboBoxConnector.js")
 @JsModule("./multiselectComboBoxConnector-es6.js")
