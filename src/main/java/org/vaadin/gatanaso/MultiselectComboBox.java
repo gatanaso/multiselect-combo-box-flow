712da839ddc453b651cf6fe0d2c64201d33b61c8
Goran <gatanaso@users.noreply.github.com>
2019-11-20 21:40:39 +0200

    increased npm package dependency version to 2.2.0

--- src/main/java/org/vaadin/gatanaso/MultiselectComboBox.java
+++ src/main/java/org/vaadin/gatanaso/MultiselectComboBox.java
@@ -81,7 +81,7 @@ import elemental.json.JsonValue;
  * @author gatanaso
  */
 @Tag("multiselect-combo-box")
-@NpmPackage(value = "multiselect-combo-box", version = "2.2.0-alpha.1")
+@NpmPackage(value = "multiselect-combo-box", version = "2.2.0")
 @JsModule("multiselect-combo-box/src/multiselect-combo-box.js")
 @JavaScript("frontend://multiselectComboBoxConnector.js")
 @JsModule("./multiselectComboBoxConnector-es6.js")
